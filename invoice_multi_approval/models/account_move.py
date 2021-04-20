2bf96227d1017c2802da87eb95ffc902521e1694
Eric Macharia <rcmacharia7@gmail.com>
2021-04-20 23:11:57 +0300

    hide approve and confirm button in accounting

--- invoice_multi_approval/models/account_move.py
+++ invoice_multi_approval/models/account_move.py
@@ -168,7 +168,7 @@ class AccountAnalyticAccount(models.Model):
     @api.depends('debit', 'credit')
     def _compute_percentage(self):
         for rec in self:
-            if not float_is_zero(rec.debit, precision_digits=2):
+            if not float_is_zero(rec.credit, precision_digits=2):
                 value = 100 * (rec.credit - rec.debit) / rec.credit
                 rec.percentage = f'{round(value,2)}%'
             else:
