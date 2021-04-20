e899df3a02b6abe6a529333c03ef3667cf6b20d6
Eric Macharia <rcmacharia7@gmail.com>
2021-04-21 01:43:11 +0300

    request validation when confirming transfers

--- stock_tier_validation/models/stock_picking.py
+++ stock_tier_validation/models/stock_picking.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
-
+from odoo.exceptions import ValidationError
 
 class StockPicking(models.Model):
     _name = "stock.picking"
     _inherit = ['stock.picking', 'tier.validation']
     _state_from = ['assigned', 'waiting', 'confirmed']
     _state_to = ['done']
+
 
     def action_done(self):
         self.write({'state': 'done'})
@@ -17,3 +18,10 @@ class StockPicking(models.Model):
 
     def _notify_accepted_reviews(self):
         return super(StockPicking, self.sudo())._notify_accepted_reviews()
+
+    def button_validate(self):
+        if not self.validated and not self.user_has_groups('stock.group_stock_manager'):
+            raise ValidationError('This transfer needs to be validated.')
+
+        res = super().button_validate()
+        return res
