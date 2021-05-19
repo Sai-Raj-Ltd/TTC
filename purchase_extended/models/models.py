8b2ef83dc074d4928520e12001705c06a0e51940
srinivasaraokagitha <81147648+srinivasaraokagitha@users.noreply.github.com>
2021-05-19 11:12:52 +0530

    Update models.py
    modifications in purchase module

--- purchase_extended/models/models.py
+++ purchase_extended/models/models.py
@@ -2,14 +2,20 @@
 
 from odoo import models, fields, api
 
-# class purchase_extended(models.Model):
-#     _name = 'purchase_extended.purchase_extended'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+    
+    priority = fields.Selection(
+        [('0', 'Normal'), ('1', 'Low'), ('2','Medium'), ('3', 'High')], 'Priority', default='0', index=True)
+    
+    
+class res_partner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+    
+    
+    status = fields.Selection(
+        [('Maker', 'Maker'), ('Checker', 'Checker')], 'Status', default='Maker', index=True)
+    
+    def make_checker(self):
+        self.status='Checker'
