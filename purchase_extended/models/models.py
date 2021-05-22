f878e1036de48cb4a2b1c405bc88f18170a82c17
srinivasaraokagitha <81147648+srinivasaraokagitha@users.noreply.github.com>
2021-05-22 23:41:39 +0530

    Update models.py
    update

--- purchase_extended/models/models.py
+++ purchase_extended/models/models.py
@@ -105,33 +105,35 @@ class PurchaseOrderLine(models.Model):
 class ProductProduct(models.Model):
     _inherit = "product.product"    
     
+    def _prepare_sellers(self, params=False):
+        return self.seller_ids.filtered(lambda s: s.name.active).sorted(lambda s: (s.name.name,s.sequence, -s.min_qty, s.price, s.id))
+
+    
     def _select_seller(self, partner_id=False, quantity=0.0, date=None, uom_id=False, params=False):
-            self.ensure_one()
-            if date is None:
-                date = fields.Date.context_today(self)
-            precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        self.ensure_one()
+        if date is None:
+            date = fields.Date.context_today(self)
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
 
-            res = self.env['product.supplierinfo']
-            sellers = self._prepare_sellers(params)
-            sellers = sellers.filtered(lambda s: not s.company_id or s.company_id.id == self.env.company.id)
-            for seller in sellers:
-                # Set quantity in UoM of seller
-                quantity_uom_seller = quantity
-                if quantity_uom_seller and uom_id and uom_id != seller.product_uom:
-                    quantity_uom_seller = uom_id._compute_quantity(quantity_uom_seller, seller.product_uom)
+        res = self.env['product.supplierinfo']
+        sellers = self._prepare_sellers(params)
+        sellers = sellers.filtered(lambda s: not s.company_id or s.company_id.id == self.env.company.id)
+        for seller in sellers:
+            # Set quantity in UoM of seller
+            quantity_uom_seller = quantity
+            if quantity_uom_seller and uom_id and uom_id != seller.product_uom:
+                quantity_uom_seller = uom_id._compute_quantity(quantity_uom_seller, seller.product_uom)
 
-                if seller.date_start and seller.date_start > date:
-                    continue
-                if seller.date_end and seller.date_end < date:
-                    continue
-                if partner_id and seller.name not in [partner_id, partner_id.parent_id]:
-                    continue
-                if float_compare(quantity_uom_seller, seller.min_qty, precision_digits=precision) == -1:
-                    continue
-                if seller.product_id and seller.product_id != self:
-                    continue
-                if not res or res.name == seller.name:
-                    res |= seller
-            res.sorted(key=lambda r: r.name, reverse=True)
-            return res
-   
+            if seller.date_start and seller.date_start > date:
+                continue
+            if seller.date_end and seller.date_end < date:
+                continue
+            if partner_id and seller.name not in [partner_id, partner_id.parent_id]:
+                continue
+            if float_compare(quantity_uom_seller, seller.min_qty, precision_digits=precision) == -1:
+                continue
+            if seller.product_id and seller.product_id != self:
+                continue
+            if not res or res.name == seller.name:
+                res |= seller
+        return res.sorted(lambda s: s.name.name)
